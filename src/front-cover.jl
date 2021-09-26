09adebab210c225e170c0a528d368068e559c74f
Jose Storopoli <43353831+storopoli@users.noreply.github.com>
2021-09-26 07:40:06 -0300

    Book Cover (#132)
    Still not the final version

--- src/front-cover.jl
+++ src/front-cover.jl
@@ -1,4 +1,6 @@
 const NOTO_SANS_BOLD = assetpath("fonts", "NotoSans-Bold.ttf")
+const JuliaColors = Colors.JULIA_LOGO_COLORS
+const Set1 = ColorSchemes.Set1_4
 Random.seed!(123)
 
 myrand(x,y,z) = rand()
@@ -40,206 +42,144 @@ Return the Julia Data Science book front cover.
 function front_cover()
     CairoMakie.activate!()
     with_theme(theme_black()) do
-        #589.44, 884.16
-        # Warning, there is not actual structure in this code,
-        # things are messy and all around.
+        # Figure
         fig = Figure(resolution=(1768,2652))
-        ax1 = Axis3(fig[1,1], perspectiveness = 0.5,  azimuth = 7.19, elevation = 0.57,
-                xlabel = "x label", ylabel = "y label", zlabel = "z label",
-                aspect = (1,1,1))
-        ax2 = Axis(fig[2,1], aspect = AxisAspect(1))
-        ax3 = Axis(fig[3,1], aspect = AxisAspect(1))
-        ax4 = Axis(fig[4,1],  aspect = AxisAspect(1))
-        ax5 = Axis(fig[5,1],  aspect = AxisAspect(1))
-        ax6 = Axis(fig[6,1],  aspect = AxisAspect(1))
-        ax7 = Axis3(fig[1,2], perspectiveness = 0.5,aspect = (1,1,1))
-        ax8 = Axis3(fig[1,3],  perspectiveness = 0.5,aspect = (1,1,1))
-        ax9 = Axis3(fig[2,2], perspectiveness = 0.5,aspect = (1,1,1))
-        ax10 = Axis3(fig[2,4], perspectiveness = 0.5,aspect = (1,1,1))
-        ax11 = Axis3(fig[2,3], perspectiveness = 0.5,aspect = (1,1,1))
-        ax12 = Axis(fig[2,3], aspect = AxisAspect(1))
-        ax13 = Axis(fig[2,6], aspect = AxisAspect(1))
-        ax14 = Axis3(fig[3,2:3], perspectiveness = 0.5, azimuth = 2.7074520285897967,
-            elevation = 0.0701990816987234, aspect = (1, 1, 0.2))
-        ax15 = Axis3(fig[4,2], perspectiveness = 0.5,aspect = (1,1,1))
-        ax16 = Axis3(fig[4,3], perspectiveness = 0.5,aspect = (1,1,1))
-
-        #ax15 = Axis3(fig[3,3], perspectiveness = 0.5, azimuth = 1π, aspect = (1,1,1))
-
-        axs = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9,
-            ax10,
-            ax11,
-            ax12, ax13,
-            ax14, ax15, ax16]
-
+        # Colors
+        colors = ColorSchemes.Set1_6
+        #colors = Makie.wong_colors()
+        # Markers
         ms = 20
-
-        meshscatter!(ax1, positions, color = vec(vals),
+        # Axis
+        ax11 = Axis3(fig[1,1], perspectiveness = 0.5,  azimuth = 7.19, elevation = 0.57,
+                 xlabel = "x label", ylabel = "y label", zlabel = "z label",
+                 xgridvisible=false, ygridvisible=false, zgridvisible=false,
+                 aspect = (1,1,1))
+        ax21 = Axis(fig[2,1], aspect = AxisAspect(1), xgridvisible=false, ygridvisible=false)
+        ax31 = Axis(fig[3,1], aspect = AxisAspect(1), xgridvisible=false, ygridvisible=false)
+        ax41 = Axis(fig[4,1],  aspect = AxisAspect(1), xgridvisible=false, ygridvisible=false)
+        ax22 = Axis3(fig[2,2], perspectiveness = 0.5, aspect = (1,1,1), xgridvisible=false, ygridvisible=false, zgridvisible=false)
+        # ax23 = Axis(fig[2,3], aspect = AxisAspect(1), xgridvisible=false, ygridvisible=false)
+        ax32 = Axis(fig[3,2], aspect = 1, xgridvisible=false, ygridvisible=false)
+        ax33 = Axis(fig[3,3], aspect = 1, xgridvisible=false, ygridvisible=false)
+        ax42 = Axis(fig[4,2], aspect = 1, xgridvisible=false, ygridvisible=false)
+        ax43 = Axis(fig[4,3], aspect = 1, xgridvisible=false, ygridvisible=false)
+        ax44 = Axis(fig[4,4], aspect = 1, xgridvisible=false, ygridvisible=false)
+        ax45 = Axis(fig[4,5], aspect = 1, xgridvisible=false, ygridvisible=false)
+        axs = [ax11, ax21, ax31, ax41,
+               ax22, #ax23,
+               ax32, ax33,
+               ax42, ax43, ax44, ax45
+              ]
+        # First Column 1,1 to 4,1
+        meshscatter!(ax11, positions, color = vec(vals),
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), # here, if you use less than 10, you will see smaller squares.
             colormap = :linear_grey_10_95_c0_n256, colorrange = (-2, 2),
             transparency = false,
             shading= false)
-
-        meshscatter!(ax1, posTops, color = vec(valsTop), marker = FRect3D(Vec3f0(0), Vec3f0(7)),
+        meshscatter!(ax11, posTops, color = vec(valsTop), marker = FRect3D(Vec3f0(0), Vec3f0(7)),
             transparency = false, colormap = (:plasma,0.65),
             shading= false, colorrange = (0,1) )
-        meshscatter!(ax1, posSides, color = vec(valsSides), marker = FRect3D(Vec3f0(0), Vec3f0(7)),
+        meshscatter!(ax11, posSides, color = vec(valsSides), marker = FRect3D(Vec3f0(0), Vec3f0(7)),
             transparency = false, colormap = (:viridis,0.65),
             shading= false, )
-
-        meshscatter!(ax2, vec(arrTop[:,end]), color = colorTop[end:-1:1], shading = false,
+        meshscatter!(ax21, vec(arrTop[:,end]), color = colorTop[end:-1:1], shading = false,
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :plasma, colorrange = (0,1))
-
-        meshscatter!(ax2, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
+        meshscatter!(ax21, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :viridis, colorrange = (0,1))
-
-        meshscatter!(ax2, vec([(j,i) for i in 0:9, j in 1:10]), shading = false,
+        meshscatter!(ax21, vec([(j,i) for i in 0:9, j in 1:10]), shading = false,
             color = vec(vals[end:-1:1,end,:]'),
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :linear_grey_10_95_c0_n256, colorrange = (-2,2))
-
-        meshscatter!(ax3, vec(arrTop[1:4,end]), color = colorTop[end:-1:1][1:4], shading = false,
+        meshscatter!(ax31, vec(arrTop[1:2,end]), color = colorTop[end:-1:1][1:2], shading = false,
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :plasma, colorrange = (0,1))
-
-        meshscatter!(ax3, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
+        meshscatter!(ax31, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :viridis, colorrange = (0,1))
-
-        meshscatter!(ax3, vec([(j,i) for i in 0:9, j in 1:4]), shading = false,
-            color = vec(vals[end:-1:1,end,:]')[1:40],
-            marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :linear_grey_10_95_c0_n256, colorrange = (-2,2))
-
-
-        meshscatter!(ax4, vec(arrTop[1:3,end]), color = colorTop[end:-1:1][1:3], shading = false,
-            marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :plasma, colorrange = (0,1))
-
-        meshscatter!(ax4, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
-            marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :viridis, colorrange = (0,1))
-
-        meshscatter!(ax4, vec([(j,i) for i in 0:9, j in 1:3]), shading = false,
-            color = vec(vals[end:-1:1,end,:]')[1:30],
-            marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :linear_grey_10_95_c0_n256, colorrange = (-2,2))
-
-        meshscatter!(ax5, vec(arrTop[1:2,end]), color = colorTop[end:-1:1][1:2], shading = false,
-            marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :plasma, colorrange = (0,1))
-
-        meshscatter!(ax5, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
-            marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :viridis, colorrange = (0,1))
-
-        meshscatter!(ax5, vec([(j,i) for i in 0:9, j in 1:2]), shading = false,
+        meshscatter!(ax31, vec([(j,i) for i in 0:9, j in 1:2]), shading = false,
             color = vec(vals[end:-1:1,end,:]')[1:20],
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :linear_grey_10_95_c0_n256, colorrange = (-2,2))
-
-
-        meshscatter!(ax6, vec(arrTop[1:1,end]), color = colorTop[end:-1:1][1:1], shading = false,
+        meshscatter!(ax41, vec(arrTop[1:1,end]), color = colorTop[end:-1:1][1:1], shading = false,
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :plasma, colorrange = (0,1))
-
-        meshscatter!(ax6, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
+        meshscatter!(ax41, vec([(0,i) for i in 0:9]), color = colorSides, shading = false,
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :viridis, colorrange = (0,1))
-        meshscatter!(ax6, vec([(j,i) for i in 0:9, j in 1:1]), shading = false,
+        meshscatter!(ax41, vec([(j,i) for i in 0:9, j in 1:1]), shading = false,
             color = vec(vals[end:-1:1,end,:]')[1:10],
             marker = FRect3D(Vec3f0(0), Vec3f0(7)), colormap = :linear_grey_10_95_c0_n256, colorrange = (-2,2))
-
-
-
-        Label(fig[1, 1, BottomLeft()], "|>", textsize = 24,
-                rotation = 0pi, padding = (0,3,8,0),font = NOTO_SANS_BOLD)
-        Label(fig[2, 1, BottomLeft()], "|>", textsize = 24,
-            rotation = 0pi, font = NOTO_SANS_BOLD)
-        Label(fig[3, 1, BottomLeft()], "|>", textsize = 24,
-            rotation = 0pi,padding = (0,3,8,0), font = NOTO_SANS_BOLD)
-
-        Label(fig[4, 1, BottomLeft()], "|>", textsize = 24,
-            rotation = 0pi, padding = (0,3,8,0), font = NOTO_SANS_BOLD)
-        Label(fig[5, 1, BottomLeft()], "|>", textsize = 24,
-            rotation = 0pi, padding = (0,3,8,0), font = NOTO_SANS_BOLD)
-
-        xlims!(ax1,-1,12)
-        ylims!(ax1,-1,11)
-        zlims!(ax1,-1,12)
-
-        xlims!(ax2,-0.5,11)
-        ylims!(ax2,-1,11)
-
-        xlims!(ax3,-0.5,11)
-        ylims!(ax3,-1,11)
-
-        xlims!(ax4,-0.5,11)
-        ylims!(ax4,-1,11)
-
-        xlims!(ax5,-0.5,11)
-        ylims!(ax5,-1,11)
-
-        xlims!(ax6,-0.5,11)
-        ylims!(ax6,-1,11)
-
-
-
-        volume!(ax7, collect(1:10),collect(1:10),collect(1:10), rand(10,10,10)) # this just works with GLMakie
-        #contour!(ax8, 1:10,1:10,1:10, rand(10,10,10))
-
+        # Limits
+        xlims!(ax11,-1,12)
+        ylims!(ax11,-1,11)
+        zlims!(ax11,-1,12)
+        xlims!(ax21,-0.5,11)
+        ylims!(ax21,-1,11)
+        xlims!(ax31,-0.5,11)
+        ylims!(ax31,-1,11)
+        xlims!(ax41,-0.5,11)
+        ylims!(ax41,-1,11)
+        # Second Columns 2,2 to 4,2
         x, y, z = peaks()
-        x2, y2, z2 = peaks(; n = 15)
-        surface!(ax9, x, y, z; colormap = :plasma)
-        #wireframe!(ax10, x2, y2, z2)
-        #contour3d!(ax11, x, y, z, levels = 15)
-        contourf!(ax12, x, y, z; colormap = :bone_1)
-        #heatmap!(ax13, x, y, z)
-        lines!(ax14, x, y, z[:,20], color = z[:,21], colormap = :Spectral_11, linewidth = 2)
-        scatter!(ax14, x, y, z[:,18], color = z[:,19], markersize = 8, colormap = :Spectral_11)
+        surface!(ax22, x, y, z; colormap = :plasma)
+        # contourf!(ax23, x, y, z; colormap = :bone_1)
         x = rand(10)
         y = rand(10)
         z = rand(10)
-        meshscatter!(ax15, x,y,z; color = fill(1,10), colormap = (:red, :white),
-            markersize = 0.1, ambient = Vec3f0(0.85, 0.85, 0.85))
-        lines!(ax15, x,y,z; color = :grey80, linewidth = 2)
-        Label(fig[3, 3:6, Bottom()], "Julia Data Science", textsize = 70,
-            tellheight = false)
-        Label(fig[4, 4:end], "Jose Storopoli, Rik Huijzer\n and Lazaro Alonso",
-            textsize = 30, tellheight = false)
-        ax62 = Axis(fig[6,2], aspect = 1)
-        ax63 = Axis(fig[6,3], aspect = 1)
-        ax64 = Axis(fig[6,4], aspect = 1)
-        ax65 = Axis(fig[6,5], aspect = 1)
-        ax52 = Axis(fig[5,2], aspect = 1)
-        ax53 = Axis(fig[5,3], aspect = 1)
-        ax66 = Axis(fig[6,6], aspect = 1)
-
-        colors = Makie.wong_colors()
-        scatter!(ax52, rand(10), rand(10); color = colors[1], markersize = 16)
-        lines!(ax53, 0..10, x -> exp(-x); color = colors[2], linewidth = 4)
-
-        hist!(ax62, randn(1000), bins = 32; color = colors[3], strokewidth = 1.5,
+        # Third Row 3,2 to to 3,3
+        scatter!(ax32, rand(10), rand(10); color = JuliaColors.blue, markersize = 16)
+        lines!(ax33, 0..10, x -> exp(-x); color = JuliaColors.red, linewidth = 4)
+        limits!(ax32, -0.5, 1.5, -0.5, 1.5)
+        limits!(ax33, -1, 15, -0.5, 1.5)
+        # Fourt Row 4,2 to 4,5
+        hist!(ax42, randn(1000), bins = 32; color = JuliaColors.blue, strokewidth = 1.5,
             strokecolor = :grey80)
-        density!(ax63, randn(1000); color = colors[4],
-            strokewidth = 2, strokecolor = colors[4])
-        violin!(ax64, fill(1,1000), randn(1000); color = (colors[5], 0.1),
-            strokewidth = 2, strokecolor = colors[5], show_median = true,)
-        boxplot!(ax65, fill(1,1000), randn(1000); color = colors[6], strokecolor = :grey80,
-            whiskercolor = colors[6], whiskerwidth = 1, strokewidth = 1)
-        scatter!(ax66, randn(1000), 1:1000; color = colors[7], markersize = 4)
-        limits!(ax52, -0.5, 1.5, -0.5, 1.5)
-        limits!(ax53, -1, 15, -0.5, 1.5)
-
-        xlims!(ax64, 0,2)
-        xlims!(ax65, 0,2)
-        ylims!(ax64, -6,6)
-        ylims!(ax65, -6,6)
-        ylims!(ax62, 0,150)
-        ylims!(ax63, 0,0.55)
-
-
-        axs = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9,
-            ax10,
-            ax11,
-            ax12, ax13,
-            ax14, ax15, ax16,
-            ax62, ax63, ax64, ax65,
-            ax52, ax53, ax66]
+        density!(ax43, randn(1000); color = JuliaColors.red,
+            strokewidth = 2, strokecolor = JuliaColors.red)
+        violin!(ax44, fill(1,1000), randn(1000); color = (JuliaColors.purple, 0.5),
+            strokewidth = 2, strokecolor = JuliaColors.purple, show_median = true,)
+        boxplot!(ax45, fill(1,1000), randn(1000); color = JuliaColors.green, strokecolor = :grey80,
+            whiskercolor = JuliaColors.green, whiskerwidth = 1, strokewidth = 1)
+        xlims!(ax44, 0,2)
+        xlims!(ax45, 0,2)
+        ylims!(ax42, 0,150)
+        ylims!(ax43, 0,0.55)
+        ylims!(ax44, -6,6)
+        ylims!(ax45, -6,6)
+        # Pipes for First Column
+        Label(fig[1, 1, Bottom()], "|>", textsize = 52,
+              rotation = -π/2, padding = (0,3,8,0),font = NOTO_SANS_BOLD)
+        Label(fig[2, 1, BottomLeft()], " |>", textsize = 52,
+              rotation = -π/2, padding = (0,3,8,0), font = NOTO_SANS_BOLD)
+        Label(fig[3, 1, BottomLeft()], " |>", textsize = 52,
+              rotation = -π/2, padding = (0,3,8,0), font = NOTO_SANS_BOLD)
+        # Pipes between columns
+        Label(fig[2,1, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        Label(fig[3,1, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        Label(fig[3,2, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        Label(fig[4,1, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        Label(fig[4,2, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        Label(fig[4,3, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        Label(fig[4,4, Right()], "|>", textsize = 40,
+              rotation = 0π, padding = (0,0,0,0), font = NOTO_SANS_BOLD)
+        # Title and Text Stuff
+        Label(fig[0, 2:5, Bottom()], "Julia\nData Science", textsize = 120,
+            tellheight = false, halign = :left)
+        Label(fig[3, 3:end], "Jose Storopoli", #color = JuliaColors.purple,
+            textsize = 60, tellheight = false, halign = :left)
+        Label(fig[3, 3:end], "\n\nRik Huijzer", #color = JuliaColors.red,
+            textsize = 60, tellheight = false, halign = :left)
+        Label(fig[3, 3:end], "\n\n\n\nLazaro Alonso", #color = JuliaColors.green,
+            textsize = 60, tellheight = false, halign = :left)
+        # Label(fig[3, 3:end], "Jose Storopoli, Rik Huijzer\n and Lazaro Alonso",
+        #     textsize = 60, tellheight = false)
+        # Final Axis and Figure touches
         [hidedecorations!(ax; grid = false) for ax in axs]
         [hidespines!(ax) for ax in axs]
         rowgap!(fig.layout, 0)
         colgap!(fig.layout, 0)
         #save("front_cover.png", fig) # no need to compress image
         #display(fig)
-        fig
+        return fig
     end
 end

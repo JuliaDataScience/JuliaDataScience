ea4aa269d008e8f47af8cefd89a3bce3a461515d
Rik Huijzer <rikhuijzer@pm.me>
2021-11-24 12:41:46 +0100

    Remove Fathom analytics (#208)

--- src/ci.jl
+++ src/ci.jl
@@ -43,7 +43,6 @@ function build()
     fail_on_error = true
     gen(; fail_on_error)
     extra_head = """
-    <script src="https://cdn.usefathom.com/script.js" data-site="EEJXHKTE" defer></script>
     <script defer type="text/javascript" src="https://api.pirsch.io/pirsch.js" id="pirschjs" data-code="eB2Isj56H4g6ndupKwaKyHau7lCkTsVV"></script>
     """
     build_all(; extra_head, fail_on_error)
